fba97f268269c591c01720f7d27fd25334c588a1
Igli Cekani <htnacekan@gmail.com>
2018-02-07 14:38:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,7 +8,8 @@ def position_taken? (board, index)
       return false
     elsif board[index]==nil
       return false
-
+    elsif board[index]=="X"||board[index]=="O"
+      return true
 
     end
 
